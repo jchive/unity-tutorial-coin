f2de1f5d9d79087b59b5ec45b394d2722bde51e2
jim <jim@hiveit.co.uk>
2018-05-02 16:48:43 +0100

    Adding script to spawn coins

--- Assets/scripts/SpawnerController.cs
+++ Assets/scripts/SpawnerController.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class SpawnerController : MonoBehaviour {
 
+	public GameObject ObjectToSpawn;
+
+	private float startTime;
+	
 	// Use this for initialization
 	void Start () {
-		
+
+		startTime = Time.time;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Time.time - startTime > 1.0f) {
+			SpawnObject();
+			startTime = Time.time;
+		}
+
 	}
+	
+	void SpawnObject() {
+		Instantiate(ObjectToSpawn, transform);
+	}
+
 }
+
