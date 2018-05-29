f492613adc5e053c50c51a7de8a2bed11c1be492
jim <jim@hiveit.co.uk>
2018-05-29 11:20:41 +0100

    Adding randomly spawing coins upon starting

---
+++ Assets/InitialCoinSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class InitialCoinSpawner : MonoBehaviour {
+    public GameObject spawnObject;
+    public int numberToSpawn = 50;
+    public Transform spawnContainer;
+
+    private void Start() {
+        gameObject.SetActive(false);
+
+        var cubeBoundary = GetComponent<Renderer>().bounds;
+
+
+        var temporarySpawn = Instantiate(spawnObject);
+        var spawnBounds = temporarySpawn.GetComponent<Renderer>().bounds;
+        var size = spawnBounds.size / 2.0f;
+
+        var spawnMinimum = cubeBoundary.min + size / 2.0f;
+        var spawnMaximum = cubeBoundary.max - size / 2.0f;
+
+        Destroy(temporarySpawn);
+
+        for (var i = 0; i < numberToSpawn; ++i) {
+            var position = new Vector3(
+                Random.Range(spawnMinimum.x, spawnMaximum.x),
+                Random.Range(spawnMinimum.y, spawnMaximum.y),
+                Random.Range(spawnMinimum.z, spawnMaximum.z)
+            );
+
+            temporarySpawn = Instantiate(spawnObject, spawnContainer);
+            temporarySpawn.transform.position = position;
+        }
+    }
+}
