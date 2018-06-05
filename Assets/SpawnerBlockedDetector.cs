7f1a26e27d68c1e2fef179d9311e35493cf2724f
jim <jim@hiveit.co.uk>
2018-06-05 14:39:31 +0100

    Extending the top

--- Assets/SpawnerBlockedDetector.cs
+++ Assets/SpawnerBlockedDetector.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class SpawnerBlockedDetector : MonoBehaviour {
+    public UnityEvent OnSpawnerBlocked = new UnityEvent();
+    public UnityEvent OnSpawerClear = new UnityEvent();
 
-	public UnityEvent OnSpawnerBlocked = new UnityEvent();
-	public UnityEvent OnSpawerClear = new UnityEvent();
+    private void OnTriggerEnter(Collider other) {
+        OnSpawnerBlocked.Invoke();
+    }
 
-	private void OnTriggerEnter(Collider other) {
-		OnSpawnerBlocked.Invoke();
-	}
-
-	private void OnTriggerExit(Collider other) {
-		OnSpawerClear.Invoke();
-	}
+    private void OnTriggerExit(Collider other) {
+        OnSpawerClear.Invoke();
+    }
 }
