05e33e5f0f0219737ee7b9bf73e0e73890835377
jim <jim@hiveit.co.uk>
2018-06-05 14:04:50 +0100

    Adding slight randomness when spawning coins to add some slight realism

--- Assets/scripts/SpawnerController.cs
+++ Assets/scripts/SpawnerController.cs
@@ -2,44 +2,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnerController : MonoBehaviour {
+    public GameObject ObjectToSpawn;
+    public Transform SpawnLocation;
 
-	public GameObject ObjectToSpawn;
-	public Transform SpawnLocation;
+    public float velocityJitter = 1.0f;
+    public float angularVelocityJitter = 10.0f;
 
-	public CoinEvent OnSpawnCoin = new CoinEvent();
+    public CoinEvent OnSpawnCoin = new CoinEvent();
 
-	public bool spawnerBlocked = false;
+    public bool spawnerBlocked = false;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+    }
 
-		if (Input.GetButtonDown("Fire1")) {
-			SpawnObject();			
-		}
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            SpawnObject();
+        }
+    }
 
-	}
-	
-	void SpawnObject() {
-		if (!spawnerBlocked) {
-			var coin = Instantiate(ObjectToSpawn, transform);
-			coin.transform.position = SpawnLocation.position;
-			coin.transform.rotation = SpawnLocation.rotation;
+    void SpawnObject() {
+        if (!spawnerBlocked) {
+            var coin = Instantiate(ObjectToSpawn, transform);
+            coin.transform.position = SpawnLocation.position;
+            coin.transform.rotation = SpawnLocation.rotation;
 
-			OnSpawnCoin.Invoke();
-		}
-	}
+            var coinRigidbody = coin.GetComponent<Rigidbody>();
 
-	public void SetSpawnerBlocked(bool value) {
-		spawnerBlocked = value;
-	}
+            coinRigidbody.angularVelocity =
+                Random.Range(-angularVelocityJitter, angularVelocityJitter) * coin.transform.up;
 
-	
+            coinRigidbody.velocity =
+                coin.transform.forward * Random.Range(-velocityJitter, velocityJitter)
+                +
+                coin.transform.right * Random.Range(-velocityJitter, velocityJitter)
+                ;
 
+
+            OnSpawnCoin.Invoke();
+        }
+    }
+
+    public void SetSpawnerBlocked(bool value) {
+        spawnerBlocked = value;
+    }
 }
-
